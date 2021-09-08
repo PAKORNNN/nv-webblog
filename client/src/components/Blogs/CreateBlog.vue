fa792ab9eff701f6b0bcf6c7ceaaf3e327335a52
PAKORNNN <phakornc62@an.ac.th>
2021-09-08 14:38:00 +0700

    new-9/8/21

--- client/src/components/Blogs/CreateBlog.vue
+++ client/src/components/Blogs/CreateBlog.vue
@@ -2,16 +2,31 @@
   <div>
     <h1>Create Blog</h1>
     <form v-on:submit.prevent="createBlog">
-      <p>title: <input type="text" v-model="blog.title" /></p>
-      <p>content: <input type="text" v-model="blog.content" /></p>
-      <p>category: <input type="text" v-model="blog.category" /></p>
-      <p>status: <input type="text" v-model="blog.status" /></p>
+      <p><strong>Title:</strong> <input type="text" v-model="blog.title" /></p>
+
+      <p><strong>Content:</strong></p>
+      <p>
+        <vue-ckeditor
+          v-model.lazy="blog.content"
+          :config="config"
+          @blur="onBlur($event)"
+          @focus="onFocus($ecent)"
+        />
+      </p>
+
+      <p>
+        <strong>Category:</strong> <input type="text" v-model="blog.category" />
+      </p>
+      <p>
+        <strong>Status:</strong> <input type="text" v-model="blog.status" />
+      </p>
       <p><button type="submit">Create Blog</button></p>
     </form>
   </div>
 </template>
 <script>
 import BlogService from "@/services/BlogService";
+import VueCkeditor from "vue-ckeditor2";
 
 export default {
   data() {
@@ -19,9 +34,16 @@ export default {
       blog: {
         title: "",
         thumbnail: "null",
-        pictues: "null", //ระวังตรงนี้คำผิด
+        pictues: "null", 
+        content: "",
         category: "",
         status: "saved",
+      },
+      config: {
+        /* toolbar: [
+          ["Bold", "Italic", "Underline", "Strike", "Subscript", "SuperScript"],
+        ], */
+        height: 300,
       },
     };
   },
@@ -36,6 +58,131 @@ export default {
         console.log(err);
       }
     },
+    onBlur(editor) {
+      console.log(editor);
+    },
+    onfocus(editor) {
+      console.log(editor);
+    },
+    created() {
+      this.config.toolbar = [
+        {
+          name: "document",
+          items: [
+            "Source",
+            "-",
+            "Save",
+            "NewPage",
+            "Preview",
+            "Print",
+            "-",
+            "Templates"
+          ]
+        },
+        {
+          name: "clipboard",
+          items: [
+            "Cut",
+            "Copy",
+            "Paste",
+            "PasteText",
+            "-",
+            "Undo",
+            "Redo"
+          ]
+        },
+        {
+          name : "editing",
+          items: ["Find", "Replace", "-", "SelectAll", "-", "Scayt"]
+        },
+        {
+          name: "forms",
+          items: [
+            "Form",
+            "Checkbox",
+            "Radio",
+            "TextField",
+            "Textarea",
+            "Select",
+            "Button",
+            "ImageButton",
+            "HiddentField"
+          ]
+        },
+        "/",
+        {
+          name: "basicstyles",
+          items: [
+            "Bold",
+            "Italic",
+            "Underline",
+            "Strike",
+            "Subscript",
+            "SuperScript",
+            "-",
+            "CopyFormatting",
+            "RemoveFormat"
+          ]
+        },
+        {
+          name: "basicstyles",
+          items: [
+            "Bold",
+            "Italic",
+            "Strike",
+            "SubScript",
+            "Superscript",
+            "-",
+            "CopyFormatting",
+            "RemoveFormat"
+          ]
+        },
+        {
+          name: "paragraph",
+          items: [
+            "NumberedList",
+            "BulletedList",
+            "-",
+            "Outdent",
+            "Indent",
+            "-",
+            "Blockquote",
+            "CreateDiv",
+            "-",
+            "JustifyLeft",
+            "JustifyCenter",
+            "JustifyRight",
+            "JustifyBlock",
+            "-",
+            "BidiLtr",
+            "BidiRtl",
+            "Language"
+          ]
+        },
+        { name : "Links", items: ["Link", "Unlink", "Anchor"]},{
+          name: "insert",
+          items: [
+            "Image",
+            "Flash",
+            "Table",
+            "HorizontalRule",
+            "Smiley",
+            "Specialchar",
+            "PageBreak",
+            "Iframe",
+            "InsertPre"
+          ]
+        },
+        "/",
+        { name : "styles", items: ["Styles", "Format", "Font", "Font", "FontSize"]},
+        { name: "colors", items:["TextColor", "BGColor"] },
+        { name: "tools", items:["Maximize", "ShowBlocks"] },
+        { name: "about", items:["About"]}
+      ]
+    }
+  },
+  components: {
+    VueCkeditor
   },
 };
 </script>
--- client/src/components/Blogs/EditBlog.vue
+++ client/src/components/Blogs/EditBlog.vue
@@ -3,7 +3,17 @@
     <h1>Edit Blog</h1>
     <form v-on:submit.prevent="editBlog">
       <p>title: <input type="text" v-model="blog.title" /></p>
-      <p>content: <input type="text" v-model="blog.content" /></p>
+
+      <p><strong>Content:</strong></p>
+      <p>
+        <vue-ckeditor
+          v-model.lazy="blog.content"
+          :config="config"
+          @blur="onBlur($event)"
+          @focus="onFocus($ecent)"
+        />
+      </p>
+
       <p>category: <input type="text" v-model="blog.category" /></p>
       <p>status: <input type="text" v-model="blog.status" /></p>
       <p>
@@ -15,6 +25,7 @@
 </template>
 <script>
 import BlogService from "@/services/BlogService";
+import VueCkeditor from "vue-ckeditor2";
 
 export default {
   data() {
@@ -22,10 +33,16 @@ export default {
       blog: {
         title: "",
         thumbnail: "null",
-        pictues: "null", //ระวังตรงนี้คำผิด
+        pictues: "null", 
         content: "",
         category: "",
         status: "",
+      },
+      config: {
+        /* toolbar: [
+          ["Bold", "Italic", "Underline", "Strike", "Subscript", "Superscript"],
+        ], */
+        height: 300
       },
     };
   },
@@ -40,14 +57,137 @@ export default {
         console.log(err);
       }
     },
+    onBlur(editor) {
+      console.log(editor);
+    },
+    onfocus(editor) {
+      console.log(editor);
+    },
   },
   async created() {
     try {
       let blogId = this.$route.params.blogId;
       this.blog = (await BlogService.show(blogId)).data;
+      this.config.toolbar = [
+        {
+          name: "document",
+          items: [
+            "Source",
+            "-",
+            "Save",
+            "NewPage",
+            "Preview",
+            "Print",
+            "-",
+            "Templates"
+          ]
+        },
+        {
+          name: "clipboard",
+          items: [
+            "Cut",
+            "Copy",
+            "Paste",
+            "PasteText",
+            "-",
+            "Undo",
+            "Redo"
+          ]
+        },
+        {
+          name : "editing",
+          items: ["Find", "Replace", "-", "SelectAll", "-", "Scayt"]
+        },
+        {
+          name: "forms",
+          items: [
+            "Form",
+            "Checkbox",
+            "Radio",
+            "TextField",
+            "Textarea",
+            "Select",
+            "Button",
+            "ImageButton",
+            "HiddentField"
+          ]
+        },
+        "/",
+        {
+          name: "basicstyles",
+          items: [
+            "Bold",
+            "Italic",
+            "Underline",
+            "Strike",
+            "Subscript",
+            "SuperScript",
+            "-",
+            "CopyFormatting",
+            "RemoveFormat"
+          ]
+        },
+        {
+          name: "basicstyles",
+          items: [
+            "Bold",
+            "Italic",
+            "Strike",
+            "SubScript",
+            "Superscript",
+            "-",
+            "CopyFormatting",
+            "RemoveFormat"
+          ]
+        },
+        {
+          name: "paragraph",
+          items: [
+            "NumberedList",
+            "BulletedList",
+            "-",
+            "Outdent",
+            "Indent",
+            "-",
+            "Blockquote",
+            "CreateDiv",
+            "-",
+            "JustifyLeft",
+            "JustifyCenter",
+            "JustifyRight",
+            "JustifyBlock",
+            "-",
+            "BidiLtr",
+            "BidiRtl",
+            "Language"
+          ]
+        },
+        { name : "Links", items: ["Link", "Unlink", "Anchor"]},{
+          name: "insert",
+          items: [
+            "Image",
+            "Flash",
+            "Table",
+            "HorizontalRule",
+            "Smiley",
+            "Specialchar",
+            "PageBreak",
+            "Iframe",
+            "InsertPre"
+          ]
+        },
+        "/",
+        { name : "styles", items: ["Styles", "Format", "Font", "Font", "FontSize"]},
+        { name: "colors", items:["TextColor", "BGColor"] },
+        { name: "tools", items:["Maximize", "ShowBlocks"] },
+        { name: "about", items:["About"]}
+      ]
     } catch (error) {
       console.log(error);
     }
+  },
+  components: {
+    VueCkeditor
   },
 };
 </script>
